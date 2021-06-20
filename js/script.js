ba2e1a3ac2323690e7ebd06897fcae2326cf1cc6
unknown <kennyolapower@yahoo.co.uk>
2021-06-20 21:52:05 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+
+function addRow() {
+          
+    var myName1 = document.getElementById("fname");
+    var myName2 = document.getElementById("lname");
+    var age = document.getElementById("age");
+    var address = document.getElementById('address');
+    var table = document.getElementById("myTableData");
+ 
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+ 
+    row.insertCell(0).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
+    row.insertCell(1).innerHTML= myName1.value;
+    row.insertCell(2).innerHTML = myName2.value;
+    row.insertCell(3).innerHTML = address.value;
+    row.insertCell(4).innerHTML= age.value;
+ 
+}
+
+function deleteRow(obj) {
+      
+    var index = obj.parentNode.parentNode.rowIndex;
+    var table = document.getElementById("myTableData");
+    table.deleteRow(index);
+    
+}
+
+function addTable() {
+    var myTableDiv = document.getElementById("myDynamicTable");
+  
+    var table = document.createElement('TABLE');
+    table.border = '1';
+  
+    var tableBody = document.createElement('TBODY');
+    table.appendChild(tableBody);
+  
+    for (var i = 0; i < 3; i++) {
+      var tr = document.createElement('TR');
+      tableBody.appendChild(tr);
+  
+      for (var j = 0; j < 4; j++) {
+        var td = document.createElement('TD');
+        td.width = '75';
+        td.appendChild(document.createTextNode("Cell " + i + "," + j));
+        tr.appendChild(td);
+      }
+    }
+    myTableDiv.appendChild(table);
+  }
+
+  
